800542322746b3b702335482b3717991179e9f0d
Chris Jensen <git@waitnostartover.com>
2020-09-23 21:23:32 +0200

    ADD: Usage sample

---
+++ Samples/Samples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+using BreadTh.StronglyApied;
+using BreadTh.StronglyApied.Attributes;
+
+namespace Samples
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = "{\"array\":[1,10,\"oh\"]}";
+
+            IEnumerable<ValidationError> errors = new ModelValidator().TryParse(input, out MyModel result);
+
+            if (errors.Count() == 0)
+                Console.WriteLine($"Valid! :D\n\n{JsonConvert.SerializeObject(result, Formatting.Indented)}");
+            else
+                Console.WriteLine($"Invalid! :c\n\n{JsonConvert.SerializeObject(errors, Formatting.Indented)}");
+        }
+
+        public class MyModel
+        {
+            [StronglyApiedArray(maxLength: 2), StronglyApiedLong(minValue: 5)]
+            public long[] @array;
+        }
+    }
+}
