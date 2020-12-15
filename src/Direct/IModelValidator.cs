be7c0d239e583faad4477241b913e61aa5677fce
Chris Jensen <git@waitnostartover.com>
2020-12-15 11:13:18 +0100

    leave stream open default value in interface

--- src/Direct/IModelValidator.cs
+++ src/Direct/IModelValidator.cs
@@ -7,9 +7,9 @@ namespace BreadTh.StronglyApied.Direct
 {
     public interface IModelValidator
     {
-        Task<(T result, List<ErrorDescription> errors)> TryParse<T>(Stream rawbody, bool leaveStreamOpen);
+        Task<(T result, List<ErrorDescription> errors)> TryParse<T>(Stream rawbody, bool leaveStreamOpen = true);
         (T result, List<ErrorDescription> errors) TryParse<T>(string rawbody);
-        Task<OUTCOME> TryParse<OUTCOME, MODEL>(Stream rawbody, Func<List<ErrorDescription>, OUTCOME> onValidationError, Func<MODEL, Task<OUTCOME>> onSuccess, bool leaveStreamOpen);
+        Task<OUTCOME> TryParse<OUTCOME, MODEL>(Stream rawbody, Func<List<ErrorDescription>, OUTCOME> onValidationError, Func<MODEL, Task<OUTCOME>> onSuccess, bool leaveStreamOpen = true);
         Task<OUTCOME> TryParse<OUTCOME, MODEL>(string rawbody, Func<List<ErrorDescription>, OUTCOME> onValidationError, Func<MODEL, Task<OUTCOME>> onSuccess);
         List<ErrorDescription> ValidateModel<T>(T value);
     }
